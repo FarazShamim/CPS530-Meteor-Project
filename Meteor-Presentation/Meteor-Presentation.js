548f69375d398ab411c1822802dcde6beca5c0bd
SlashHarken <kevin.Furtado@live.ca>
2015-11-14 19:13:38 -0500

    Half-done the Meteor Tutorial
    This is a Half done tutorial session

--- Meteor-Presentation/Meteor-Presentation.js
+++ Meteor-Presentation/Meteor-Presentation.js
@@ -1,18 +1,66 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Resolutions = new Mongo.Collection('resolutions');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+  //This code runs in the client webpage
+  Template.body.helpers({
+    resolutions: function(){
+      //give reolosiutions if it is unchecked in a session
+      if(Session.get('hideFinished')){
+        return Resolutions.find({checked: {$ne: true}});
+      }else{
+        return Resolutions.find();
+      }
+    },
+
+    //Makes sure that the sessign variable it synced
+    hideFinished: function() {
+      return Session.get('hideFinished');
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  //how to call any event in a meteor project
+  Template.body.events({
+    //this is a function when the form is submitted this happens (triggered by events)
+    'submit .new-resolution': function(event) {
+      //variable holding the Tile of the form 
+      var title = event.target.title.value;
+
+      //add record to database (Store the title )
+      Resolutions.insert({
+        title: title, 
+        createdAt: new Date()
+      });
+
+      //clear the form (the text)
+      event.target.title.value ="";
+
+      //This prevents a page refeesh due to the submition of the form 
+      return false;
     }
+    ,//Remeber the god dam comma
+    'change .hide-finished': function (event){
+      Session.set('hideFinished', event.target.checked)
+    }
+  });
+
+  Template.resolution.events({
+
+    //check box clicked event - stores the checked state
+    'click .toggle-checked': function () {
+      Resolutions.update(this._id, {$set: {checked: !this.checked}});
+    }
+    , // <- need this comma between functions
+    //click event since its a button!
+    'click .delete': function (){
+      //remove the element from the db (_id)
+      //remove this id that this is attached to 
+      Resolutions.remove(this._id); 
+    }
+
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
